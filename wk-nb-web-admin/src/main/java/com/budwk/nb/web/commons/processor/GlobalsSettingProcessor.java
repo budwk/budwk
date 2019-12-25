323fc606530f2a94ec29ac66a758ef1699303dae
Wizzercn <wizzer@qq.com>
2019-12-25 21:57:25 +0800

    :memo: 增加跨域配置项

--- wk-nb-web-admin/src/main/java/com/budwk/nb/web/commons/processor/GlobalsSettingProcessor.java
+++ wk-nb-web-admin/src/main/java/com/budwk/nb/web/commons/processor/GlobalsSettingProcessor.java
@@ -1,17 +1,32 @@
 package com.budwk.nb.web.commons.processor;
 
+import org.nutz.ioc.impl.PropertiesProxy;
 import org.nutz.lang.Strings;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
 import org.nutz.mvc.ActionContext;
+import org.nutz.mvc.ActionInfo;
 import org.nutz.mvc.Mvcs;
+import org.nutz.mvc.NutConfig;
 import org.nutz.mvc.impl.processor.AbstractProcessor;
 
 /**
- * @author wizzer(wizzer@qq.com) on 2016/6/22.
+ * @author wizzer(wizzer @ qq.com) on 2016/6/22.
  */
 public class GlobalsSettingProcessor extends AbstractProcessor {
     private static final Log log = Logs.get();
+    private PropertiesProxy conf;
+    private boolean ACCESS_ALLOW_ORIGIN_ENABLED;
+    private String ACCESS_ALLOW_ORIGIN_MAXAGE;
+    private String ACCESS_ALLOW_ORIGIN_DOMAIN;
+
+    @Override
+    public void init(NutConfig config, ActionInfo ai) throws Throwable {
+        conf = config.getIoc().get(PropertiesProxy.class, "conf");
+        ACCESS_ALLOW_ORIGIN_ENABLED = conf.getBoolean("budwk.access.allow.origin.enabled", false);
+        ACCESS_ALLOW_ORIGIN_DOMAIN = conf.get("budwk.access.allow.origin.domain", "127.0.0.1");
+        ACCESS_ALLOW_ORIGIN_MAXAGE = conf.get("budwk.access.allow.origin.maxage", "0");
+    }
 
     @Override
     @SuppressWarnings("rawtypes")
@@ -27,6 +42,13 @@ public class GlobalsSettingProcessor extends AbstractProcessor {
             lang = Mvcs.getLocalizationKey();
         }
         ac.getRequest().setAttribute("lang", lang);
+        if (ACCESS_ALLOW_ORIGIN_ENABLED) {
+            ac.getResponse().addHeader("Access-Control-Allow-Origin", ACCESS_ALLOW_ORIGIN_DOMAIN);
+            ac.getResponse().addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+            ac.getResponse().addHeader("Access-Control-Allow-Headers",
+                    "X-Requested-With,X-Token");
+            ac.getResponse().addHeader("Access-Control-Max-Age", ACCESS_ALLOW_ORIGIN_MAXAGE);
+        }
         doNext(ac);
     }
 
