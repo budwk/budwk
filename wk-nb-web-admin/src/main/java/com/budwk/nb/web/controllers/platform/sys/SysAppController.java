2feedf89b38ab191e9aecce5cefaddb74d7aff31
Wizzercn <wizzer@qq.com>
2020-01-15 15:17:57 +0800

    update: 完成应用管理功能

--- wk-nb-web-admin/src/main/java/com/budwk/nb/web/controllers/platform/sys/SysAppController.java
+++ wk-nb-web-admin/src/main/java/com/budwk/nb/web/controllers/platform/sys/SysAppController.java
@@ -22,16 +22,18 @@ import org.nutz.integration.jedis.RedisService;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.json.Json;
+import org.nutz.lang.Streams;
 import org.nutz.lang.Strings;
+import org.nutz.lang.stream.StringInputStream;
 import org.nutz.lang.util.NutMap;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
-import org.nutz.mvc.annotation.ApiVersion;
-import org.nutz.mvc.annotation.At;
-import org.nutz.mvc.annotation.Ok;
-import org.nutz.mvc.annotation.Param;
+import org.nutz.mvc.Mvcs;
+import org.nutz.mvc.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
 import java.util.*;
 
 /**
@@ -267,6 +269,362 @@ public class SysAppController {
             loglevelService.changeLoglevel(loglevelCommand);
             return Result.success();
         } catch (Exception e) {
+            return Result.error();
+        }
+    }
+
+    /**
+     * @api {get} /api/1.0.0/platform/sys/app/jar/list 获取Jar包列表
+     * @apiName jar/list
+     * @apiGroup SYS_APP
+     * @apiPermission sys.server.app.jar
+     * @apiVersion 1.0.0
+     * @apiParam {String} appName   实例名
+     * @apiParam {String} pageNo   页码
+     * @apiParam {String} pageSize   页大小
+     * @apiParam {String} pageOrderName   排序字段
+     * @apiParam {String} pageOrderBy   排序方式
+     * @apiSuccess {Number} code  0
+     * @apiSuccess {String} msg   操作成功
+     */
+    @At("/jar/list")
+    @Ok("json:{locked:'password|salt',ignoreNull:false}")
+    @RequiresPermissions("sys.server.app.jar")
+    public Object jarData(@Param("appName") String appName, @Param("pageNo") int pageNo, @Param("pageSize") int pageSize, @Param("pageOrderName") String pageOrderName, @Param("pageOrderBy") String pageOrderBy) {
+        try {
+            Cnd cnd = Cnd.NEW();
+            if (Strings.isNotBlank(appName)) {
+                cnd.and("appName", "like", "%" + appName + "%");
+            }
+            if (Strings.isNotBlank(pageOrderName) && Strings.isNotBlank(pageOrderBy)) {
+                cnd.orderBy(pageOrderName, PageUtil.getOrder(pageOrderBy));
+            }
+            return Result.success().addData(sysAppListService.listPageLinks(pageNo, pageSize, cnd, "^(user)$"));
+        } catch (Exception e) {
+            return Result.error();
+        }
+    }
+
+    /**
+     * @api {post} /api/1.0.0/platform/sys/app/jar/create 添加Jar包
+     * @apiName jar/create
+     * @apiGroup SYS_APP
+     * @apiPermission sys.server.app.jar.create
+     * @apiVersion 1.0.0
+     * @apiParam {Object} sysAppList   表单对象
+     * @apiSuccess {Number} code  0
+     * @apiSuccess {String} msg   操作成功
+     */
+    @At("/jar/create")
+    @Ok("json")
+    @POST
+    @RequiresPermissions("sys.server.app.jar.create")
+    @SLog(tag = "添加安装包", msg = "应用名称:${sysAppList.appName}")
+    public Object jarCreate(@Param("..") Sys_app_list sysAppList, HttpServletRequest req) {
+        try {
+            int num = sysAppListService.count(Cnd.where("appName", "=", Strings.trim(sysAppList.getAppName())).and("appVersion", "=", Strings.trim(sysAppList.getAppVersion())));
+            if (num > 0) {
+                return Result.error("sys.server.app.txt.version.exist");
+            }
+            sysAppList.setAppName(Strings.trim(sysAppList.getAppName()));
+            sysAppList.setAppVersion(Strings.trim(sysAppList.getAppVersion()));
+            sysAppList.setCreatedBy(StringUtil.getPlatformUid());
+            sysAppList.setUpdatedBy(StringUtil.getPlatformUid());
+            sysAppListService.insert(sysAppList);
+            return Result.success();
+        } catch (Exception e) {
+            return Result.error();
+        }
+    }
+
+    /**
+     * @api {post} /api/1.0.0/platform/sys/app/jar/search 查询Jar包
+     * @apiName jar/search
+     * @apiGroup SYS_APP
+     * @apiPermission sys.server.app.jar
+     * @apiVersion 1.0.0
+     * @apiParam {String} appName   实例名
+     * @apiSuccess {Number} code  0
+     * @apiSuccess {String} msg   操作成功
+     */
+    @At("/jar/search")
+    @Ok("json")
+    @RequiresPermissions("sys.server.app.jar")
+    public Object jarSearch(@Param("appName") String appName) {
+        return Result.NEW().addData(sysAppListService.getAppNameList());
+    }
+
+    /**
+     * @api {post} /api/1.0.0/platform/sys/app/jar/delete/:id 删除Jar包
+     * @apiName jar/delete
+     * @apiGroup SYS_APP
+     * @apiPermission sys.server.app.jar.delete
+     * @apiVersion 1.0.0
+     * @apiParam {String} id   ID
+     * @apiSuccess {Number} code  0
+     * @apiSuccess {String} msg   操作成功
+     */
+    @At("/jar/delete/?")
+    @Ok("json")
+    @RequiresPermissions("sys.server.app.jar.delete")
+    @SLog(tag = "删除Jar包")
+    public Object jarDelete(String id, HttpServletRequest req) {
+        try {
+            Sys_app_list appList = sysAppListService.fetch(id);
+            if (appList == null) {
+                return Result.error("system.error.noData");
+            }
+            sysAppListService.delete(id);
+            req.setAttribute("_slog_msg", appList.getAppName());
+            return Result.success();
+        } catch (Exception e) {
+            return Result.error();
+        }
+    }
+
+    /**
+     * @api {post} /api/1.0.0/platform/sys/app/jar/disabled 启用禁用Jar包
+     * @apiName jar/disabled
+     * @apiGroup SYS_APP
+     * @apiPermission sys.server.app.jar.update
+     * @apiVersion 1.0.0
+     * @apiParam {String} id   ID
+     * @apiParam {String} disabled   true/false
+     * @apiSuccess {Number} code  0
+     * @apiSuccess {String} msg   操作成功
+     * @apiSuccess {Object} data  数据
+     */
+    @At("/jar/disabled")
+    @Ok("json")
+    @POST
+    @RequiresPermissions("sys.server.app.jar.update")
+    @SLog(tag = "启用禁用Jar包")
+    public Object changeDisabled(@Param("id") String id, @Param("disabled") boolean disabled, HttpServletRequest req) {
+        try {
+            sysAppListService.update(Chain.make("disabled", disabled), Cnd.where("id", "=", id));
+            if (disabled) {
+                req.setAttribute("_slog_msg", Mvcs.getMessage(req, "system.commons.txt.disabled.on"));
+            } else {
+                req.setAttribute("_slog_msg", Mvcs.getMessage(req, "system.commons.txt.disabled.off"));
+            }
+            return Result.success();
+        } catch (Exception e) {
+            log.error(e);
+            return Result.error();
+        }
+    }
+
+    /**
+     * @api {get} /api/1.0.0/platform/sys/app/conf/list 获取配置列表
+     * @apiName conf/list
+     * @apiGroup SYS_APP
+     * @apiPermission sys.server.app.conf
+     * @apiVersion 1.0.0
+     * @apiParam {String} appName   实例名
+     * @apiParam {String} pageNo   页码
+     * @apiParam {String} pageSize   页大小
+     * @apiParam {String} pageOrderName   排序字段
+     * @apiParam {String} pageOrderBy   排序方式
+     * @apiSuccess {Number} code  0
+     * @apiSuccess {String} msg   操作成功
+     */
+    @At("/conf/list")
+    @Ok("json:{locked:'confData|password|salt',ignoreNull:false}")
+    @RequiresPermissions("sys.server.app.conf")
+    public Object confData(@Param("confName") String confName, @Param("pageNo") int pageNo, @Param("pageSize") int pageSize, @Param("pageOrderName") String pageOrderName, @Param("pageOrderBy") String pageOrderBy) {
+        try {
+            Cnd cnd = Cnd.NEW();
+            if (Strings.isNotBlank(confName)) {
+                cnd.and("confName", "like", "%" + confName + "%");
+            }
+            if (Strings.isNotBlank(pageOrderName) && Strings.isNotBlank(pageOrderBy)) {
+                cnd.orderBy(pageOrderName, PageUtil.getOrder(pageOrderBy));
+            }
+            return Result.success().addData(sysAppConfService.listPageLinks(pageNo, pageSize, cnd, "^(user)$"));
+        } catch (Exception e) {
+            return Result.error();
+        }
+    }
+
+    /**
+     * @api {post} /api/1.0.0/platform/sys/app/conf/create 添加Jar包
+     * @apiName conf/create
+     * @apiGroup SYS_APP
+     * @apiPermission sys.server.app.conf.create
+     * @apiVersion 1.0.0
+     * @apiParam {Object} sysAppConf   表单对象
+     * @apiSuccess {Number} code  0
+     * @apiSuccess {String} msg   操作成功
+     */
+    @At("/conf/create")
+    @Ok("json")
+    @RequiresPermissions("sys.server.app.conf.create")
+    @SLog(tag = "添加配置文件", msg = "应用名称:${sysAppConf.confName}")
+    public Object confAddDo(@Param("..") Sys_app_conf sysAppConf, HttpServletRequest req) {
+        try {
+            int num = sysAppConfService.count(Cnd.where("confName", "=", Strings.trim(sysAppConf.getConfName())).and("confVersion", "=", Strings.trim(sysAppConf.getConfVersion())));
+            if (num > 0) {
+                return Result.error("sys.server.app.txt.version.exist");
+            }
+            sysAppConf.setConfName(Strings.trim(sysAppConf.getConfName()));
+            sysAppConf.setConfVersion(Strings.trim(sysAppConf.getConfVersion()));
+            sysAppConf.setCreatedBy(StringUtil.getPlatformUid());
+            sysAppConf.setUpdatedBy(StringUtil.getPlatformUid());
+            sysAppConfService.insert(sysAppConf);
+            return Result.success();
+        } catch (Exception e) {
+            return Result.error();
+        }
+    }
+
+    /**
+     * @api {post} /api/1.0.0/platform/sys/app/conf/search 查询配置
+     * @apiName conf/search
+     * @apiGroup SYS_APP
+     * @apiPermission sys.server.app.conf
+     * @apiVersion 1.0.0
+     * @apiParam {Object} sysAppConf   表单对象
+     * @apiSuccess {Number} code  0
+     * @apiSuccess {String} msg   操作成功
+     */
+    @At("/conf/search")
+    @Ok("json")
+    @RequiresPermissions("sys.server.app.conf")
+    public Object confSearch(@Param("confName") String confName) {
+        return Result.NEW().addData(sysAppConfService.getConfNameList());
+    }
+
+    /**
+     * @api {post} /api/1.0.0/platform/sys/app/conf/delete/:id 删除配置
+     * @apiName conf/delete
+     * @apiGroup SYS_APP
+     * @apiPermission sys.server.app.conf.delete
+     * @apiVersion 1.0.0
+     * @apiParam {String} id   ID
+     * @apiSuccess {Number} code  0
+     * @apiSuccess {String} msg   操作成功
+     */
+    @At("/conf/delete/?")
+    @Ok("json")
+    @RequiresPermissions("sys.server.app.conf.delete")
+    @SLog(tag = "删除配置文件")
+    public Object confDelete(String id, HttpServletRequest req) {
+        try {
+            Sys_app_conf appConf = sysAppConfService.fetch(id);
+            if (appConf == null) {
+                return Result.error("system.error.noData");
+            }
+            sysAppConfService.delete(id);
+            req.setAttribute("_slog_msg", appConf.getConfName());
+            return Result.success();
+        } catch (Exception e) {
+            return Result.error();
+        }
+    }
+
+    /**
+     * @api {post} /api/1.0.0/platform/sys/app/conf/download/:id 下载配置文件
+     * @apiName download/delete
+     * @apiGroup SYS_APP
+     * @apiPermission sys.server.app.conf
+     * @apiVersion 1.0.0
+     * @apiParam {String} id   ID
+     * @apiSuccess {Object} data  文件流
+     */
+    @At("/conf/download/?")
+    @Ok("void")
+    @RequiresPermissions("sys.server.app.conf")
+    public void confDownload(String id, HttpServletResponse response) {
+        try {
+            Sys_app_conf conf = sysAppConfService.fetch(id);
+            String fileName = conf.getConfName() + "-" + conf.getConfVersion() + ".properties";
+            response.setHeader("Content-Type", "text/properties");
+            response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
+            try (InputStream in = new StringInputStream(conf.getConfData())) {
+                Streams.writeAndClose(response.getOutputStream(), in);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * @api {post} /api/1.0.0/platform/sys/app/conf/get/:id 获取配置内容
+     * @apiName conf/get
+     * @apiGroup SYS_APP
+     * @apiPermission sys.server.app.conf
+     * @apiVersion 1.0.0
+     * @apiParam {String} id   ID
+     * @apiSuccess {Number} code  0
+     * @apiSuccess {String} msg   操作成功
+     */
+    @At("/conf/get/?")
+    @Ok("json")
+    @RequiresPermissions("sys.server.app.conf")
+    public Object confGet(String id, HttpServletRequest req) {
+        try {
+            Sys_app_conf appConf = sysAppConfService.fetch(id);
+            if (appConf == null) {
+                return Result.error("system.error.noData");
+            }
+            return Result.success().addData(appConf);
+        } catch (Exception e) {
+            return Result.error();
+        }
+    }
+
+    /**
+     * @api {post} /api/1.0.0/platform/sys/app/conf/update 修改配置
+     * @apiName conf/update
+     * @apiGroup SYS_APP
+     * @apiPermission sys.server.app.conf.update
+     * @apiVersion 1.0.0
+     * @apiParam {Object} sysAppConf   表单对象
+     * @apiSuccess {Number} code  0
+     * @apiSuccess {String} msg   操作成功
+     */
+    @At("/conf/update")
+    @Ok("json")
+    @RequiresPermissions("sys.server.app.conf.update")
+    @SLog(tag = "修改配置文件", msg = "应用名称:${sysAppConf.confName}")
+    public Object confEditDo(@Param("..") Sys_app_conf sysAppConf, HttpServletRequest req) {
+        try {
+            sysAppConf.setUpdatedBy(StringUtil.getPlatformUid());
+            sysAppConfService.updateIgnoreNull(sysAppConf);
+            return Result.success();
+        } catch (Exception e) {
+            return Result.error();
+        }
+    }
+
+    /**
+     * @api {post} /api/1.0.0/platform/sys/app/conf/disabled 启用禁用Jar包
+     * @apiName conf/disabled
+     * @apiGroup SYS_APP
+     * @apiPermission sys.server.app.conf.update
+     * @apiVersion 1.0.0
+     * @apiParam {String} id   ID
+     * @apiParam {String} disabled   true/false
+     * @apiSuccess {Number} code  0
+     * @apiSuccess {String} msg   操作成功
+     * @apiSuccess {Object} data  数据
+     */
+    @At("/conf/disabled")
+    @Ok("json")
+    @POST
+    @RequiresPermissions("sys.server.app.conf.update")
+    @SLog(tag = "启用禁用Jar包")
+    public Object changeConfDisabled(@Param("id") String id, @Param("disabled") boolean disabled, HttpServletRequest req) {
+        try {
+            sysAppConfService.update(Chain.make("disabled", disabled), Cnd.where("id", "=", id));
+            if (disabled) {
+                req.setAttribute("_slog_msg", Mvcs.getMessage(req, "system.commons.txt.disabled.on"));
+            } else {
+                req.setAttribute("_slog_msg", Mvcs.getMessage(req, "system.commons.txt.disabled.off"));
+            }
+            return Result.success();
+        } catch (Exception e) {
+            log.error(e);
             return Result.error();
         }
     }
