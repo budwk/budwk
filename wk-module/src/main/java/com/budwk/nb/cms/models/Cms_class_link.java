0d5e0b56a53cda91302279412dedb72221037d48
wizzer <wizzer@qq.com>
2020-11-04 17:34:02 +0800

    add: @ManyMany 多对多关联表创建POJO类便于做数据库集群

---
+++ wk-module/src/main/java/com/budwk/nb/cms/models/Cms_class_link.java
@@ -0,0 +1,30 @@
+package com.budwk.nb.cms.models;
+
+import com.budwk.nb.commons.base.model.BaseModel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.nutz.dao.entity.annotation.*;
+
+import java.io.Serializable;
+
+/**
+ * @author wizzer@qq.com
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Table("cms_class_link")
+public class Cms_class_link extends BaseModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Column
+    @Id
+    private long id;
+
+    @Column
+    @ColDefine(type = ColType.VARCHAR, width = 32)
+    private String classId;
+
+    @Column
+    @ColDefine(type = ColType.VARCHAR, width = 32)
+    private String linkId;
+}
