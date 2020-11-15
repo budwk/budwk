c5b26a0a34c7651def95b79fc28a188cd02c1c09
wizzer <wizzer@qq.com>
2020-11-15 15:57:19 +0800

    update: 继续完善redis集群操作

--- wk-nb-web-admin/src/main/java/com/budwk/nb/web/commons/ext/websocket/WkWebSocket.java
+++ wk-nb-web-admin/src/main/java/com/budwk/nb/web/commons/ext/websocket/WkWebSocket.java
@@ -17,6 +17,9 @@ import javax.websocket.EndpointConfig;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.budwk.nb.commons.constants.RedisConstant.REDIS_KEY_WSROOM;
 
 /**
@@ -44,23 +47,27 @@ public class WkWebSocket extends AbstractWsEndpoint implements PubSub {
         roomProvider = new WkJedisRoomProvider(jedisAgent, REDIS_KEY_SESSION_TTL);
         if (jedisAgent.isClusterMode()) {
             JedisCluster jedisCluster = jedisAgent.getJedisClusterWrapper().getJedisCluster();
+            List<String> keys = new ArrayList<>();
             for (JedisPool pool : jedisCluster.getClusterNodes().values()) {
                 try (Jedis jedis = pool.getResource()) {
                     ScanParams match = new ScanParams().match(REDIS_KEY_WSROOM + "*");
                     ScanResult<String> scan = null;
                     do {
                         scan = jedis.scan(scan == null ? ScanParams.SCAN_POINTER_START : scan.getStringCursor(), match);
-                        for (String key : scan.getResult()) {
-                            switch (jedis.type(key)) {
-                                case "none":
-                                    break;
-                                case "set":
-                                    break;
-                                default:
-                                    jedis.del(key);
-                            }
-                        }
+                        keys.addAll(scan.getResult());
                     } while (!scan.isCompleteIteration());
+                }
+            }
+            try (Jedis jedis = jedisAgent.getResource()) {
+                for (String key : keys) {
+                    switch (jedis.type(key)) {
+                        case "none":
+                            break;
+                        case "set":
+                            break;
+                        default:
+                            jedis.del(key);
+                    }
                 }
             }
         } else {
