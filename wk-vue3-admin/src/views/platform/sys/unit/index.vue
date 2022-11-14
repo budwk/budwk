53345d8cce3d1ffe78eea30f4b598ca6388ac2a2
Wizzer <wizzer@qq.com>
2022-11-14 17:34:11 +0800

    vue3: 单位管理

--- wk-vue3-admin/src/views/platform/sys/unit/index.vue
+++ wk-vue3-admin/src/views/platform/sys/unit/index.vue
@@ -5,8 +5,7 @@
                 <el-input v-model="queryParams.name" placeholder="请输入单位名称" clearable @keyup.enter="handleSearch" />
             </el-form-item>
             <el-form-item label="部门负责人" prop="leaderName">
-                <el-input
-v-model="queryParams.leaderName" placeholder="请输入部门负责人" clearable
+                <el-input v-model="queryParams.leaderName" placeholder="请输入部门负责人" clearable
                     @keyup.enter="handleSearch" />
             </el-form-item>
             <el-form-item>
@@ -16,82 +15,185 @@ v-model="queryParams.leaderName" placeholder="请输入部门负责人" clearabl
         </el-form>
         <el-row :gutter="10" class="mb8">
             <el-col :span="1.5">
-                <el-button
-type="primary" @click="handleCreate"
-                    v-permission="['sys.manage.unit.create']">新增</el-button>
+                <el-button type="primary" @click="handleCreate" v-permission="['sys.manage.unit.create']">新增</el-button>
             </el-col>
             <el-col :span="1.5">
-                <el-button type="info" @click="toggleExpandAll">{{isExpandAll?'折叠':'展开'}}</el-button>
+                <el-button type="info" @click="toggleExpandAll">{{ isExpandAll ? '折叠' : '展开' }}</el-button>
             </el-col>
-            <right-toolbar
-v-model:showSearch="showSearch" :extendSearch="true" :columns="columns"
+            <right-toolbar v-model:showSearch="showSearch" :extendSearch="true" :columns="columns"
                 @quickSearch="quickSearch" :quickSearchShow="true" quickSearchPlaceholder="通过单位名称搜索" />
         </el-row>
 
-        <el-table
-            v-if="refreshTable" 
-            v-loading="tableLoading" 
-            :data="tableData" 
-            row-key="id"
-            :default-expand-all="isExpandAll" 
-            :tree-props="{ children: 'children', hasChildren: 'hasChildren' }">
-            <template v-for="(item,idx) in columns" :key="idx">
-                <el-table-column
-:prop="item.prop" :label="item.label" :fixed="item.fixed"
-                v-if="item.show" :show-overflow-tooltip="true">
-                    <template v-if="item.prop=='type'" #default="scope">
+        <el-table v-if="refreshTable" v-loading="tableLoading" :data="tableData" row-key="id"
+            :default-expand-all="isExpandAll" :tree-props="{ children: 'children', hasChildren: 'hasChildren' }">
+            <template v-for="(item, idx) in columns" :key="idx">
+                <el-table-column :prop="item.prop" :label="item.label" :fixed="item.fixed" v-if="item.show"
+                    :show-overflow-tooltip="true">
+                    <template v-if="item.prop == 'name'" #default="scope">
+                        <i v-if="scope.row.type && scope.row.type.value === 'GROUP'" class="fa fa-building" />
+                        <i v-if="scope.row.type && scope.row.type.value === 'COMPANY'" class="fa fa-home" />
+                        {{ scope.row.name }}
+                    </template>
+                    <template v-if="item.prop == 'type'" #default="scope">
                         <span>{{ scope.row.type?.text }}</span>
                     </template>
-                    <template v-if="item.prop=='createdAt'" #default="scope">
+                    <template v-if="item.prop == 'createdAt'" #default="scope">
                         <span>{{ formatTime(scope.row.createdAt) }}</span>
                     </template>
-                    <template v-if="item.prop=='disabled'" #default="scope">
-                        <span>{{ scope.row.disabled }}</span>
+                    <template v-if="item.prop == 'disabled'" #default="scope">
+                        <el-tag v-if="scope.row.disabled" type="danger">禁用</el-tag>
+                        <el-tag v-else type="success">启用</el-tag>
                     </template>
                 </el-table-column>
             </template>
             <el-table-column fixed="right" label="操作" class-name="small-padding fixed-width">
                 <template #default="scope">
-                    <el-button
-link type="primary" @click="handleUpdate(scope.row)"
+                    <el-button link type="primary" @click="handleUpdate(scope.row)"
                         v-permission="['sys.manage.unit.update']">修改</el-button>
-                    <el-button
-link type="primary" icon="Plus" @click="handleCreate(scope.row)"
+                    <el-button link type="primary" icon="Plus" @click="handleCreate(scope.row)"
                         v-permission="['sys.manage.unit.create']">新增</el-button>
-                    <el-button
-v-if="scope.row.path != '0001'" link type="danger" icon="Delete" @click="handleDelete(scope.row)"
-                        v-permission="['sys.manage.unit.delete']">删除</el-button>
+                    <el-button v-if="scope.row.path != '0001'" link type="danger" icon="Delete"
+                        @click="handleDelete(scope.row)" v-permission="['sys.manage.unit.delete']">删除</el-button>
                 </template>
             </el-table-column>
         </el-table>
+
+        <el-dialog title="新增单位" v-model="showCreate" width="50%" append-to-body>
+            <el-form ref="createRef" :model="formData" :rules="formRules" label-width="120px">
+                <el-row>
+                    <el-col :span="24">
+                        <el-form-item label="上级单位" prop="parentId">
+                            <el-tree-select v-model="formData.parentId" :data="unitOptions"
+                                :props="{ value: 'id', label: 'name', children: 'children' }" value-key="id"
+                                placeholder="选择上级单位" check-strictly style="width:100%"/>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="12">
+                        <el-form-item label="单位名称" prop="name">
+                            <el-input v-model="formData.name" placeholder="请输入单位名称" maxlength="100" />
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="12">
+                        <el-form-item label="单位类型" prop="type">
+                            <el-radio-group v-model="formData.type">
+                                <el-radio v-for="item in types" :key="item.value" :label="item.value">
+                                    {{ item.label }}
+                                </el-radio>
+                            </el-radio-group>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="12">
+                        <el-form-item label="单位别名" prop="aliasName">
+                            <el-input v-model="formData.aliasName" placeholder="单位别名" maxlength="100"
+                                auto-complete="off" type="text" />
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="12">
+                        <el-form-item label="单位编码" prop="unitcode">
+                            <el-input v-model="formData.unitcode" placeholder="单位编码" maxlength="32" auto-complete="off"
+                                type="text" />
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="12" v-if="formData.type !== 'UNIT'">
+                        <el-form-item label="单位地址" prop="address">
+                            <el-input v-model="formData.address" placeholder="单位详细地址" auto-complete="off" type="text" />
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="12" v-if="formData.type !== 'UNIT'">
+                        <el-form-item label="固定电话" prop="telephone">
+                            <el-input v-model="formData.telephone" placeholder="单位固定电话" auto-complete="off"
+                                type="text" />
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="12" v-if="formData.type !== 'UNIT'">
+                        <el-form-item label="电子邮箱" prop="email">
+                            <el-input v-model="formData.email" placeholder="单位电子邮箱" auto-complete="off" type="text" />
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="12" v-if="formData.type !== 'UNIT'">
+                        <el-form-item label="单位网站" prop="website">
+                            <el-input v-model="formData.website" placeholder="单位网站" auto-complete="off" type="text" />
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="12" v-if="formData.type == 'UNIT'">
+                        <el-form-item label="部门负责人" prop="leaderName">
+                            <el-input v-model="formData.leaderName" placeholder="负责人姓名" auto-complete="off"
+                                type="text" />
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="12" v-if="formData.type == 'UNIT'">
+                        <el-form-item label="负责人电话" prop="leaderMobile">
+                            <el-input v-model="formData.leaderMobile" placeholder="负责人电话" auto-complete="off"
+                                type="text" />
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="24">
+                        <el-form-item prop="note" label="备注">
+                            <el-input v-model="formData.note" type="textarea" />
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+            </el-form>
+            <template #footer>
+                <div class="dialog-footer">
+                    <el-button type="primary" @click="create">确 定</el-button>
+                    <el-button @click="showCreate = false">取 消</el-button>
+                </div>
+            </template>
+        </el-dialog>
     </div>
 </template>
 <script setup lang="ts">
 import { nextTick, onMounted, reactive, ref, toRefs } from 'vue'
-import { getList } from '/@/api/platform/sys/unit'
+import { ElForm } from 'element-plus'
+import { doCreate, getList } from '/@/api/platform/sys/unit'
 import { handleTree } from '/@/utils/common'
+import { buildValidatorData } from '/@/utils/validate'
 
+const queryRef = ref<InstanceType<typeof ElForm>>()
+const createRef = ref<InstanceType<typeof ElForm>>()
+const updateRef = ref<InstanceType<typeof ElForm>>()
 const isExpandAll = ref(true)
 const showSearch = ref(false)
 const refreshTable = ref(true)
 const tableLoading = ref(false)
 const tableData = ref([])
+const unitOptions = ref([]);
+const showCreate = ref(false)
+
 
 const data = reactive({
-    formData: {},
+    formData: {
+        id: '',
+        parentId: '',
+        name: '',
+        aliasName: '',
+        type: 'UNIT',
+        unitcode: '',
+        address: '',
+        telephone: '',
+        email: '',
+        website: '',
+        leaderName: '',
+        leaderMobile: '',
+        note: ''
+    },
+    types: [
+        { value: 'COMPANY', label: '分公司' },
+        { value: 'UNIT', label: '部门' }
+    ],
     queryParams: {
         name: '',
         leaderName: '',
     },
     formRules: {
         parentId: [{ required: true, message: "上级部门不能为空", trigger: "blur" }],
-        deptName: [{ required: true, message: "部门名称不能为空", trigger: "blur" }],
-        orderNum: [{ required: true, message: "显示排序不能为空", trigger: "blur" }],
-        email: [{ type: "email", message: "请输入正确的邮箱地址", trigger: ["blur", "change"] }],
-        phone: [{ pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/, message: "请输入正确的手机号码", trigger: "blur" }]
+        name: [{ required: true, message: "单位名称不能为空", trigger: "blur" }],
+        email: [buildValidatorData({ name: 'email' })],
+        leaderMobile: [buildValidatorData({ name: 'mobile' })]
     },
 })
-const { queryParams, formData, formRules } = toRefs(data)
+const { queryParams, formData, formRules, types } = toRefs(data)
 
 const columns = ref([
     { prop: 'name', label: `单位名称`, show: true, fixed: false },
@@ -103,13 +205,34 @@ const columns = ref([
 
 const listPage = () => {
     tableLoading.value = true
-    getList(queryParams.value).then((res)=>{
+    getList(queryParams.value).then((res) => {
         tableLoading.value = false
         tableData.value = handleTree(res.data) as never
+        unitOptions.value = handleTree(res.data) as never
     })
-} 
+}
 
-onMounted(()=>{
+const resetForm = (formEl: InstanceType<typeof ElForm> | undefined) => {
+    if (!formEl) return
+    formData.value = {
+        id: '',
+        parentId: '',
+        name: '',
+        aliasName: '',
+        type: 'UNIT',
+        unitcode: '',
+        address: '',
+        telephone: '',
+        email: '',
+        website: '',
+        leaderName: '',
+        leaderMobile: '',
+        note: ''
+    }
+    formEl?.resetFields()
+}
+
+onMounted(() => {
     listPage()
 })
 
@@ -124,7 +247,8 @@ const handleSearch = () => {
 }
 
 const resetSearch = () => {
-    
+    queryRef.value?.resetFields()
+    listPage()
 }
 
 const toggleExpandAll = () => {
@@ -136,7 +260,8 @@ const toggleExpandAll = () => {
 }
 
 const handleCreate = (row: any) => {
-
+    resetForm(createRef.value)
+    showCreate.value = true
 }
 
 const handleUpdate = (row: any) => {
@@ -147,13 +272,18 @@ const handleDelete = (row: any) => {
 
 }
 
+const create = () => {
+
+}
+
 </script>
+<!--定义组件名用于keep-alive页面缓存-->
 <script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
+export default {
     name: 'platform-sys-unit'
-})
+}
 </script>
+<!--定义布局-->
 <route lang="yaml">
     meta:
       layout: platform/index
