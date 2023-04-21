31c5ea3447e21414e871523b2618cdb62e5d216b
Wizzer <wizzer@qq.com>
2023-04-21 15:45:02 +0800

    update: wk-device-server & 修复 wk-ucenter 验证码bug

--- wk-device/wk-device-server/src/main/java/com/budwk/app/device/WkDeviceLauncher.java
+++ wk-device/wk-device-server/src/main/java/com/budwk/app/device/WkDeviceLauncher.java
@@ -1,7 +1,36 @@
 package com.budwk.app.device;
 
+import lombok.extern.slf4j.Slf4j;
+import org.nutz.boot.NbApp;
+import org.nutz.dao.Dao;
+import org.nutz.ioc.Ioc;
+import org.nutz.ioc.impl.PropertiesProxy;
+import org.nutz.ioc.loader.annotation.Inject;
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.mvc.adaptor.JsonAdaptor;
+import org.nutz.mvc.annotation.AdaptBy;
+
 /**
  * @author wizzer.cn
  */
+@IocBean(create = "init")
+@AdaptBy(type = JsonAdaptor.class)
+@Slf4j
 public class WkDeviceLauncher {
+    @Inject("refer:$ioc")
+    private Ioc ioc;
+    @Inject
+    private PropertiesProxy conf;
+    @Inject
+    private Dao dao;
+
+    public static void main(String[] args) throws Exception {
+        NbApp nb = new NbApp().setArgs(args).setPrintProcDoc(true);
+        nb.getAppContext().setMainPackage("com.budwk");
+        nb.run();
+    }
+
+    public void init() {
+
+    }
 }
---
+++ wk-device/wk-device-server/src/main/java/com/budwk/app/device/controllers/pub/PubDemoController.java
@@ -0,0 +1,12 @@
+package com.budwk.app.device.controllers.pub;
+
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.mvc.annotation.At;
+
+/**
+ * @author wizzer.cn
+ */
+@IocBean
+@At("/pub/demo")
+public class PubDemoController {
+}
