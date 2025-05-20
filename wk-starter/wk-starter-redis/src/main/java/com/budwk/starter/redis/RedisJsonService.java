a24b52d53cec480212ebb3b931f578275676a9dd
wizzer <wizzer@MacBook-Air.local>
2025-05-20 10:41:45 +0800

    update: RedisJsonService 改成动态获取连接，以支持双活切换

--- wk-starter/wk-starter-redis/src/main/java/com/budwk/starter/redis/RedisJsonService.java
+++ wk-starter/wk-starter-redis/src/main/java/com/budwk/starter/redis/RedisJsonService.java
@@ -6,57 +6,52 @@ import redis.clients.jedis.UnifiedJedis;
 import redis.clients.jedis.json.Path;
 import redis.clients.jedis.json.Path2;
 
-@IocBean(create = "init")
+@IocBean
 public class RedisJsonService {
     @Inject
     private RedisStarter redisStarter;
-    private UnifiedJedis client;
-
-    public void init() {
-        client = redisStarter.getUnifiedJedis();
-    }
 
     public <T> String setObject(String key, T object) {
-        return client.jsonSet(key, Path.ROOT_PATH, object);
+        return redisStarter.getUnifiedJedis().jsonSet(key, Path.ROOT_PATH, object);
     }
 
     public <T> String updateField(String key, String path, T value) {
-        return client.jsonSetWithEscape(key, Path2.of(path), value);
+        return redisStarter.getUnifiedJedis().jsonSetWithEscape(key, Path2.of(path), value);
     }
 
     public long delObject(String key) {
-        return client.jsonDel(key);
+        return redisStarter.getUnifiedJedis().jsonDel(key);
     }
 
     public long delField(String key, String path) {
-        return client.jsonDel(key, Path2.of(path));
+        return redisStarter.getUnifiedJedis().jsonDel(key, Path2.of(path));
     }
 
     public <T> T getObject(String key, Class<T> clazz) {
-        return client.jsonGet(key, clazz);
+        return redisStarter.getUnifiedJedis().jsonGet(key, clazz);
     }
 
     public Object getField(String key, String path) {
-        return client.jsonGet(key, Path2.of(path));
+        return redisStarter.getUnifiedJedis().jsonGet(key, Path2.of(path));
     }
 
     public String setEx(String key, String value, int expire) {
-        return client.setex(key, expire, value);
+        return redisStarter.getUnifiedJedis().setex(key, expire, value);
     }
 
     public String get(String key) {
-        return client.get(key);
+        return redisStarter.getUnifiedJedis().get(key);
     }
 
     public long del(String key) {
-        return client.del(key);
+        return redisStarter.getUnifiedJedis().del(key);
     }
 
     public long hset(String key, String field, String value) {
-        return client.hset(key, field, value);
+        return redisStarter.getUnifiedJedis().hset(key, field, value);
     }
 
     public boolean exist(String key) {
-        return client.exists(key);
+        return redisStarter.getUnifiedJedis().exists(key);
     }
 }
